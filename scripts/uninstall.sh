241758b25cbdbfa89a102a1df53ba870a9e99d28
chojuninengu <chojuninengu@gmail.com>
2026-01-26 16:28:57 +0100

    chore: Update legacy uninstaller URL to reference the v1.5.0 tag.

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -88,7 +88,7 @@ if [ "$OS" = "linux" ]; then
 fi
 
 # Remote script URLs and temporary directory
-LEGACY_UNINSTALL_URL="https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-suricata/suricata-modular-scripts/scripts/uninstall.sh"
+LEGACY_UNINSTALL_URL="https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-suricata/v1.5.0/scripts/uninstall.sh"
 TMP_DIR=$(mktemp -d)
 
 # Cleanup function for temporary directory
